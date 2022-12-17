0078fb7b4b15da0135104d107a67bed1ec60ac6c
Debashish Patra <suvam0451@gmail.com>
2022-12-17 23:12:50 +0530

    chore: add searchbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,149 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
+
+class Test extends StatefulWidget {
+  const Test({super.key});
+
+  @override
+  State<Test> createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My First App Segs"),
+        centerTitle: true,
+        backgroundColor: Colors.lightGreen.shade600,
+        elevation: 0,
+      ),
+      body: Center(
+          child: Stack(
+        fit: StackFit.loose,
+        children: [
+          TextButton(
+            onPressed: () {
+              if (kDebugMode) {
+                print("hello");
+              }
+              // print("pressed")
+            },
+            child: const Text("click me"),
+            // icon: Icon(Icons.web),
+          ),
+          ElevatedButton.icon(
+            icon: const Icon(Icons.mail, color: Colors.black),
+            onPressed: (() {
+              if (kDebugMode) {
+                print("pressed");
+              }
+            }),
+            label:
+                const Text("Contact Us", style: TextStyle(color: Colors.black)),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.amber,
+            ),
+          ),
+        ],
+      )),
+      //     child: Icon(
+      //   Icons.search,
+      //   color: Colors.amber,
+      //   size: 50,
+      // )),
+      // Image(image: NetworkImage('https://via.placeholder.com/350x150'))),
+      floatingActionButton: FloatingActionButton(
+        child: const Text("+"),
+        onPressed: () => {},
+      ),
+    );
+  }
+}
+
+class CustomSearchDelegate extends SearchDelegate {
+// Demo list to show querying
+  List<String> searchTerms = [
+    "Apple",
+    "Banana",
+    "Mango",
+    "Pear",
+    "Watermelons",
+    "Blueberries",
+    "Pineapples",
+    "Strawberries"
+  ];
+
+// first overwrite to
+// clear the search text
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () {
+          query = '';
+        },
+        icon: const Icon(Icons.clear),
+      ),
+    ];
+  }
+
+// second overwrite to pop out of search menu
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, null);
+      },
+      icon: const Icon(Icons.arrow_back),
+    );
+  }
+
+// third overwrite to show query result
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var fruit in searchTerms) {
+      if (fruit.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(fruit);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+    );
+  }
+
+// last overwrite to show the
+// querying process at the runtime
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var fruit in searchTerms) {
+      if (fruit.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(fruit);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+    );
+  }
+}
 
 void main() async {
   // Set up the SettingsController, which will glue user settings to multiple
@@ -16,5 +157,7 @@ void main() async {
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
   // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+  // runApp(MyApp(settingsController: settingsController));
+
+  runApp(const MaterialApp(home: Test()));
 }
